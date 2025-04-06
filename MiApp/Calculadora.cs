ceb335e20b56e904fff51d558f4d1d1dfda2a517
Angel-de-la-rosa <angeldelarosa.trabajo@gmail.com>
2025-04-06 13:43:01 +0200

    primer commit

---
+++ MiApp/Calculadora.cs
@@ -0,0 +1,6 @@
+﻿namespace MiApp;
+
+public class Calculadora
+{
+    public int Restar(int a, int b) => a - b;
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿namespace DemoTest;
+using MiApp;
+public class UnitTest1
+{
+    [Fact]
+    public void Resstar_CincoMenosTres_EsDos()
+    {
+        var calc = new Calculadora();
+        int resultado = calc.Restar(5, 3);
+        Assert.Equal(2, resultado);
+    }
+}
